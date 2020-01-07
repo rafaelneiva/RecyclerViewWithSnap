0f48895590c0e7db7811942a9058c62031fb298f
rafaelneiva <rafael@zup.com.br>
2020-01-07 18:45:32 -0300

    callback when item selected

--- RecylerSnap/app/src/main/java/com/rafaelneiva/myapplication/MainActivity.kt
+++ RecylerSnap/app/src/main/java/com/rafaelneiva/myapplication/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.SmoothScroller.ScrollVectorProvider
+import androidx.recyclerview.widget.SnapHelper
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnSnapPositionChangeListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +22,16 @@ class MainActivity : AppCompatActivity() {
         rv.adapter = CustomAdapter()
         rv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
-        SnapHelperOneByOne().attachToRecyclerView(rv)
+        val snapHelper = SnapHelperOneByOne()
+        snapHelper.attachToRecyclerView(rv)
+
+        val snapOnScrollListener =
+            SnapOnScrollListener(
+                snapHelper,
+                SnapOnScrollListener.Behavior.NOTIFY_ON_SCROLL_STATE_IDLE,
+                this
+            )
+        rv.addOnScrollListener(snapOnScrollListener)
 
     }
 
@@ -66,6 +77,58 @@ class MainActivity : AppCompatActivity() {
             return if (currentPosition == RecyclerView.NO_POSITION) {
                 RecyclerView.NO_POSITION
             } else currentPosition
+
         }
     }
+
+    class SnapOnScrollListener(
+        private val snapHelper: SnapHelper,
+        var behavior: Behavior = Behavior.NOTIFY_ON_SCROLL,
+        var onSnapPositionChangeListener: OnSnapPositionChangeListener? = null
+    ) : RecyclerView.OnScrollListener() {
+
+        enum class Behavior {
+            NOTIFY_ON_SCROLL,
+            NOTIFY_ON_SCROLL_STATE_IDLE
+        }
+
+        private var snapPosition = RecyclerView.NO_POSITION
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            if (behavior == Behavior.NOTIFY_ON_SCROLL) {
+                maybeNotifySnapPositionChange(recyclerView)
+            }
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            if (behavior == Behavior.NOTIFY_ON_SCROLL_STATE_IDLE
+                && newState == RecyclerView.SCROLL_STATE_IDLE
+            ) {
+                maybeNotifySnapPositionChange(recyclerView)
+            }
+        }
+
+        private fun maybeNotifySnapPositionChange(recyclerView: RecyclerView) {
+            val snapPosition = snapHelper.getSnapPosition(recyclerView)
+            val snapPositionChanged = this.snapPosition != snapPosition
+            if (snapPositionChanged) {
+                onSnapPositionChangeListener?.onSnapPositionChange(snapPosition)
+                this.snapPosition = snapPosition
+            }
+        }
+
+        fun SnapHelper.getSnapPosition(recyclerView: RecyclerView): Int {
+            val layoutManager = recyclerView.layoutManager ?: return RecyclerView.NO_POSITION
+            val snapView = findSnapView(layoutManager) ?: return RecyclerView.NO_POSITION
+            return layoutManager.getPosition(snapView)
+        }
+    }
+
+    override fun onSnapPositionChange(position: Int) {
+        Toast.makeText(this, "Position: $position", Toast.LENGTH_SHORT).show()
+    }
+}
+
+interface OnSnapPositionChangeListener {
+    fun onSnapPositionChange(position: Int)
 }
